5bf8146fb7d58b2ed0639d117fbde01f40786ec4
王勇 <wy19945372694@163.com>
2023-08-08 10:08:07 +0800

    add 数组解构.js

---
+++ 数组解构.js
@@ -0,0 +1,16 @@
+// const [x, y] = [1, 2]
+
+// console.log(x); // 1
+// console.log(y); // 2
+
+// const [x, ...y] = [1, 2, 3, 4, 5]
+// console.log(x) // 1
+// console.log(y) // [ 2, 3, 4, 5 ]
+
+// const [x, y = 2] = [1]
+// console.log(x) // 1
+// console.log(y) // 2
+
+// const [x, [y]] = [1, [2]]
+// console.log(x) // 1
+// console.log(y) // 2
