4b537ad730a2568e2e70c60633e3cab722844ad6
王勇 <wy19945372694@163.com>
2023-08-21 19:59:26 +0800

    add some files

---
+++ 函数提升优先于变量提升的举例说明.js
@@ -0,0 +1,13 @@
+console.log(foo)
+/* 
+   打印结果：
+   ƒ foo() {
+       return "abc"
+   }
+ */
+
+var foo = "foo"
+
+function foo() {
+    return "abc"
+}
---
+++ 函数提升的举例解释.js
@@ -0,0 +1,15 @@
+// 原代码
+
+hoisted()
+
+function hoisted(){
+    console.log("Hoisted! ");
+}
+
+// 可被理解为
+
+function hoisted(){
+    console.log("Hoisted! ");
+}
+
+hoisted()
